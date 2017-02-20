6400b3ccae5c35d0fe8961d431c704132ab79267
Nathan K Levis <levisnathan@gmail.com>
2017-02-20 03:43:31 +0000

    adding models

---
+++ models.py
@@ -0,0 +1,15 @@
+import flask_sqlalchemy
+import app
+
+app.app.config['SQLALCHEMY_DATABASE_URL'] = 'postgresql://nlevis:Nate@67923@localhost/postgres'
+db = flask_sqlalchemy.SQLAlchemy(app.app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120))
+    
+    def __init__(self, t):
+        self.text = t
+        
+    def __repr__(self):
+        return '<Message text: %s>' % self.text
